9489e4b64a1f99844cbb3ac4c8fe63cede9f05cc
c9u11 <tjrdud6412@naver.com>
2021-11-30 23:44:03 +0900

    Styles

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Detail from "./routes/Detail";
-import Home from "./routes/Home";
+import Detail from "./routes/js/Detail";
+import Home from "./routes/js/Home";
+import "./App.css";
 
 function App() {
   return (
